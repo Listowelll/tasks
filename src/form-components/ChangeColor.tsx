4d1fc85a40f1d08b082e244719278f4e9e410748
LordListowel <animlistowel@gmail.com>
2024-03-08 10:28:28 -0500

    First test run

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const colors = [
+        "red",
+        "blue",
+        "green",
+        "yellow",
+        "orange",
+        "purple",
+        "pink",
+        "cyan",
+        "brown",
+        "black"
+    ];
+
+    const [selectedColor, setSelectedColor] = useState<string>("");
+
+    const handleColorChange = (color: string) => {
+        setSelectedColor(color);
+    };
+
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((color, index) => (
+                <Form.Check
+                    key={index}
+                    type="radio"
+                    name="color"
+                    id={`color-check-${index}`}
+                    label={color}
+                    inline
+                    onChange={() => handleColorChange(color)}
+                    checked={selectedColor === color}
+                />
+            ))}
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "100px",
+                    height: "100px",
+                    backgroundColor: selectedColor,
+                    marginTop: "10px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    color: "white"
+                }}
+            >
+                {selectedColor}
+            </div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,42 @@
 import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [requestedAttempts, setRequestedAttempts] = useState<string>("");
+
+    const handleUseAttempt = () => {
+        setAttemptsLeft(attemptsLeft - 1);
+    };
+
+    const handleGainAttempt = () => {
+        const parsedAttempts = parseInt(requestedAttempts);
+        if (!isNaN(parsedAttempts)) {
+            setAttemptsLeft(attemptsLeft + parsedAttempts);
+            setRequestedAttempts("");
+        }
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRequestedAttempts(event.target.value);
+    };
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>Attempts Left: {attemptsLeft}</div>
+            <Form.Group controlId="formAttempts">
+                <Form.Label>Requested Attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requestedAttempts}
+                    onChange={handleInputChange}
+                />
+            </Form.Group>
+            <Button onClick={handleUseAttempt} disabled={attemptsLeft <= 0}>
+                Use
+            </Button>
+            <Button onClick={handleGainAttempt}>Gain</Button>
         </div>
     );
 }
